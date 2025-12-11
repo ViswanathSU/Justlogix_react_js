a573cc9c6720659702834017351ac32bf54d9a3f
ViswanathSU <viswanath.suresh14@gmail.com>
2025-12-11 10:29:45 +0530

    first commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,9 +11,6 @@ function LoginForm({theme}) {
   const [remember, setRemember] = useState(false);
   const [savedEmail, setSavedEmail] = useState("");
 
-  console.log(remember,"rem",savedEmail,"saveemail");
-  
-
   // for remember me
   const email = Cookies.get("userEmail");
   useEffect(() => {

9713cad36a853a5caf1e62d63a0deb01f6297915
ViswanathSU <viswanath.suresh14@gmail.com>
2025-12-18 10:55:01 +0530

    Add Formik and Yup setup for forms

---
+++ tests/login.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('viswanath14@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Viswa@2004');
+  await page.getByRole('checkbox', { name: 'Remember me' }).check();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Login' }).click();
+});
